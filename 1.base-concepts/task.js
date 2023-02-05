c3bd90bbee5a101c33193043966d4343a781518c
Alex01 <faithinlove5.an@gmail.com>
2023-02-06 00:19:17 +0300

    my commi for task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let disc = Math.pow(b, 2) - 4 * a * c;
+
+  if (disc === 0) {
+    arr.push(-b / (2 * a));
+  }
+  else if (disc > 0) {
+    arr.push((-b + Math.sqrt(disc)) / (2 * a));
+    arr.push((-b - Math.sqrt(disc)) / (2 * a));
+  }
   return arr;
 }
 
