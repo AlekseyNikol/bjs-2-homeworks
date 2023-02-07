17bbdeca6e87cb37c887d959272cd4306a1ba554
Alex01 <faithinlove5.an@gmail.com>
2023-02-08 01:18:48 +0300

    decision exercise

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,23 +1,66 @@
 function getArrayParams(...arr) {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  let sum = 0;
+  //console.log(max)
+
+  arr.forEach(arr => {
+    sum += arr;
+  })
+
+  let avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
-
+//---------------------------------------------------------
 function summElementsWorker(...arr) {
 
+  let sumValue;
+
+  sumValue = arr.reduce((a, b) => a + b);
+
+  return sumValue
+   
 }
 
 function differenceMaxMinWorker(...arr) {
+  let maxValue = Math.max(...arr);
+  let minValue = Math.min(...arr);
 
+  return maxValue - minValue
 }
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      sumOddElement += arr[i];
+    }
+    else {
+      sumEvenElement += arr[i];
+    }
+  }
+
+  return sumEvenElement - sumOddElement
 
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+  }
+
+  return sumEvenElement / countEvenElement
 }
+//-----------------------------------------------
 
 function makeWork (arrOfArr, func) {
 
