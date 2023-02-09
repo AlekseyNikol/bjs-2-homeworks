98677e593a369733f1bb0020b2c7475c26a536ad
Alex01 <faithinlove5.an@gmail.com>
2023-02-09 22:46:32 +0300

    array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,5 +6,28 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let filters;
+    let sumAge;
+    let result;
+    if (users.length == 0) {
+        result = 0;
+    } else if (gender == "мужской") {
+        filters = users.filter(users => users.gender == "мужской")
+        //console.log(filters)
+
+        sumAge = filters.map((filters, sum) => sum += filters.age - sum);
+        //console.log(sumAge)
+
+        result = sumAge.reduce((a, b) => (a + b)) / sumAge.length
+
+    } else if (gender == "женский") {
+        filters = users.filter(users => users.gender == "женский")
+
+        sumAge = filters.map((filters, sum) => sum += filters.age - sum);
+
+        result = sumAge.reduce((a, b) => (a + b)) / sumAge.length
+    } else {
+        result = 0;
+    }
+    return result
 }
