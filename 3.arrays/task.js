be65394adddf7b0da3feac4c659cb531768f6ae8
Alex01 <faithinlove5.an@gmail.com>
2023-02-09 20:10:15 +0300

    task first

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+    let comperison;
+    comperison = arr1.length === arr2.length && arr1.every((n,i) => n == arr2[i])
+
+  return comperison
 }
 
 function getUsersNamesInAgeRange(users, gender) {
