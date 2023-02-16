d1e82735362d1a564f8bdbaf8ee068a6d90359d2
Alex01 <faithinlove5.an@gmail.com>
2023-02-16 19:15:10 +0300

    commit in objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,17 +1,25 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if ('marks' in this === true) {
+        return 
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks.length === 0) {
+        return 0
+    }
+    return this.marks.reduce((acc, item, arr) => acc + item / arr.length, 0)
 }
 
 Student.prototype.exclude = function (reason) {
