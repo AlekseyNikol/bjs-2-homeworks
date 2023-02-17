dc81b6e6fa9e1e3cf448f5311183c793e6aa3201
Alex01 <faithinlove5.an@gmail.com>
2023-02-17 17:18:59 +0300

    correct tasks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,7 +2,7 @@ function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
-    this.marks = [];
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -10,8 +10,8 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if ('marks' in this) {
-        this.marks = 
+    if ('marks' in this === false) {
+        this.marks = [marks]
         //правильно ли осуществленна проверка свойств?
         //как на этой стадии добавить элементы(оценки) в существующий массив marks
     }
