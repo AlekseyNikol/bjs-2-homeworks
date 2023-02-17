21d2f67f114a8eaf887e2b2e7c536f391e6c82a2
Alex01 <faithinlove5.an@gmail.com>
2023-02-17 18:39:05 +0300

    correct tasks5

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,19 +10,22 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if ('marks' in this === false) {
-        this.marks = [marks]
-        //правильно ли осуществленна проверка свойств?
-        //как на этой стадии добавить элементы(оценки) в существующий массив marks
+    if ("marks" in this === true) {
+        this.marks = [...this.marks, ...marks]
     }
 }
 
+Student.prototype.addMarks(mark1, mark2, mark3, ...rest) = function (marks) {
+    return addMarks;
+}
+
 Student.prototype.getAverage = function () {
-    if ('marks' in this === fals || this.marks.length === 0) {
-        //проверка на существования объекта marks и наличия в нем оценок.
+    if ('marks' in this === false || this.marks.length === 0) {
+        //правильно ли проверка на существования объекта marks и наличия в нем оценок?
         return 0
     }
-    return this.marks.reduce((acc, item, arr) => acc + item / arr.length, 0)
+    return this.marks.reduce((acc, item) => acc + item / this.marks.length, 0)
+    //правильно ли мщется среднее значение у this.marks?
 }
 
 Student.prototype.exclude = function (reason) {
