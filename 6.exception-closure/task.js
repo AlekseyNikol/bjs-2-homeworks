0d9505428bdc213930c43d9461ec0bc1978aa4be
AlekseyNikol <faithinlove5.an@gmail.com>
2023-03-23 21:01:23 +0300

    first task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,19 @@
-﻿
+﻿// Задание 1
+
+function parseCount(num) {
+   const parsed = Number.parseFloat(num)
+   if (Number.isNaN(parsed)) {
+      throw new Error('Невалидное значение');
+   }
+   return parsed
+}
+
+function validateCount(num) {
+   try {
+      return parseCount(num)
+   } catch (error) {
+      return error
+   }
+}
+
+
