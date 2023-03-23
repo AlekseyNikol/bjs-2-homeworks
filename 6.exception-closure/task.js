9fbbe580a7b69eadd8888221f3fb693af5971069
AlekseyNikol <faithinlove5.an@gmail.com>
2023-03-23 21:30:48 +0300

    second task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,4 +16,39 @@ function validateCount(num) {
    }
 }
 
+// Задание 2
 
+class Triangle {
+   constructor(a, b, c) {
+      if (a + b < c || b + c < a || a + c < b) {
+         throw new Error('Треугольник с такими сторонами не существует')
+      }
+      this.a = a
+      this.b = b
+      this.c = c
+   }
+
+   get perimeter() {
+      return this.a + this.b + this.c
+   }
+
+   get area() {
+      const halfPerimetr = this.perimeter * 0.5
+      return Number(Math.sqrt(halfPerimetr * (halfPerimetr - this.a) * (halfPerimetr - this.b) * (halfPerimetr - this.c)).toFixed(3))
+   }
+}
+
+function getTriangle(a, b, c) {
+   try {
+      return new Triangle(a, b, c)
+   } catch (error) {
+      return {
+         get area() {
+            return "Ошибка! Треугольник не существует"
+         },
+         get perimeter() {
+            return "Ошибка! Треугольник не существует"
+         }
+      }
+   }
+}
