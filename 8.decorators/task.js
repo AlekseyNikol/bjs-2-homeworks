7dceb1ab58ff3d43f6cf2f27434097ef712f1834
AlekseyNikol <faithinlove5.an@gmail.com>
2023-05-13 21:39:41 +0300

    one task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,33 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+   let cache = [];
+
+   function wrapper(...args) {
+      const hach = md5(args);
+      let objectInCache = cache.find((item) => item.hach === hach);
+      if (objectInCache) {
+         console.log('Из кэша: ' + objectInCache.value)
+         return 'Из кэша: ' + objectInCache.value;
+      }
+
+      let result = func(...args);
+      const cacheResult = {
+         hach,
+         value: result
+      };
+
+      cache.push(cacheResult)
+      if (cache.length > 5) {
+         cache.shift()
+      }
+
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+   }
+   return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+
 }
